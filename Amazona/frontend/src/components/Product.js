841d3fdb34126d81415627cf4d11bd54e20e0f82
sougata <sdas15226@gmail,com>
2021-09-22 23:59:19 +0530

    cartin redux

--- Amazona/frontend/src/components/Product.js
+++ Amazona/frontend/src/components/Product.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
 export default function Product(props) {
   const { products } = props;
   return (
     <div key={products._id} className="card">
-      <a href={`/product/${products._id}`}>
+      <Link to={`/product/${products._id}`}>
         <img className="medium" src={products.images} alt={products.name} />
-      </a>
+      </Link>
       <div className="card-body">
-        <a href={`/product/${products._id}`}>
+        <Link to={`/product/${products._id}`}>
           <h2>{products.name}</h2>
-        </a>
+        </Link>
       </div>
       <Rating
         rating={products.rating}
