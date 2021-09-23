7d948552d86219f2c0ed350885b45d72f063e799
sougata <sdas15226@gmail,com>
2021-09-23 23:27:44 +0530

    shoping cart proceed to check

--- Amazona/frontend/src/actions/cartActions.js
+++ Amazona/frontend/src/actions/cartActions.js
@@ -7,12 +7,12 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
     type: CART_ADD_ITEM,
     payload: {
       name: data.name,
-      image: data.image,
+      image: data.images,
       price: data.price,
       countInStock: data.countInStock,
       product: data._id,
       qty,
     },
   });
-  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
