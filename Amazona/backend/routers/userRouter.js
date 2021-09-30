6f7ea715af2bf61cbcf324cf725ac6ed0fc21681
sougata <sdas15226@gmail,com>
2021-09-30 14:12:10 +0530

    create Register screen

--- Amazona/backend/routers/userRouter.js
+++ Amazona/backend/routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import data from "../data.js";
 import User from "../models/userModel.js";
 import { generateToken } from "../utils.js";
@@ -32,7 +32,26 @@ userRouter.post(
         return;
       }
     }
-    res.status(401).send({message: 'Invalid email or password' });
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(createdUser),
+    });
   })
 );
 
--- Amazona/frontend/src/screens/CartScreen.js
+++ Amazona/frontend/src/screens/CartScreen.js
@@ -17,7 +17,7 @@ export default function CartScreen(props) {
   }, [dispatch, productId, qty]);
 
   const checkoutHandler = () => {
-    props.history.push("/signing?redirect =shipping");
+    props.history.push("/signin?redirect=shipping");
   };
 
   const removeFromCartHandler = (id) => {
