2c4d8976957c56bfb7ba50196808cc27ee3d9d3a
sougata <sdas15226@gmail,com>
2021-10-10 12:01:42 +0530

    List Products

--- Amazona/frontend/src/App.js
+++ Amazona/frontend/src/App.js
@@ -14,6 +14,8 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SigninScreen from "./screens/SigninScreen";
 import PrivateRoute from "./components/PrivateRoute";
+import AdminRoute from "./components/AdminRoute";
+import ProductListScreen from "./screens/ProductListScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -98,11 +100,12 @@ function App() {
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/order/:id" component={OrderScreen}></Route>
           <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          
           <PrivateRoute
             path="/profile"
             component={ProfileScreen}
           ></PrivateRoute>
-
+          <AdminRoute path="/productlist" component={ProductListScreen}></AdminRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row foot">All Right Reserved</footer>
