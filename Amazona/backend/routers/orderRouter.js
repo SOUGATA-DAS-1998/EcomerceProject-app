bf40bc8f6350c612badfd3d870b2923755729f36
sougata <sdas15226@gmail,com>
2021-10-21 23:53:24 +0530

    Delete Orders in Admin section

--- Amazona/backend/routers/orderRouter.js
+++ Amazona/backend/routers/orderRouter.js
@@ -10,7 +10,7 @@ orderRouter.get(
   isAuth,
   isAdmin,
   expressAsyncHandler(async (req, res) => {
-    const orders = await Order.find({}).populate('user','name');
+    const orders = await Order.find({}).populate("user", "name");
     res.send(orders);
   })
 );
@@ -83,4 +83,20 @@ orderRouter.put(
     }
   })
 );
+
+orderRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      const deleteOrder = await order.remove();
+      res.send({ message: " Order Deleted", order: deleteOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
 export default orderRouter;
--- Amazona/frontend/src/store.js
+++ Amazona/frontend/src/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
-import { orderCreateReducer, orderDetailsReducer, orderListReducer, orderMineListReducer, orderPayReducer } from "./reducers/orderReducers";
+import { orderCreateReducer, orderDeleteReducer, orderDetailsReducer, orderListReducer, orderMineListReducer, orderPayReducer } from "./reducers/orderReducers";
 //import data from './data';
 import {
   productCreateReducer,
@@ -45,6 +45,7 @@ const reducer = combineReducers({
   productUpdate:productUpdateReducer,
   productDelete: productDeleteReducer,
   orderList: orderListReducer,
+  orderDelete: orderDeleteReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
